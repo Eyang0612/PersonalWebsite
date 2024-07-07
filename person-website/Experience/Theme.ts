33b1b37016031b513e3b14d87d6e21b6709f9c8e
Eyang0612 <eddieyang0612@gmail.com>
2024-07-07 21:44:43 +0800

    Fixed minor Theme CSS bug

--- person-website/Experience/Theme.ts
+++ person-website/Experience/Theme.ts
@@ -25,22 +25,21 @@ export default class Theme extends EventEmitter {
             const icon = document.body.querySelector("#theme-button i")
             icon.classList.toggle("fa-sun")
             icon.classList.toggle("fa-moon")
-            // if(this.theme === "dark"){
+            if(this.theme === "dark"){
                 
-            //     btnOutline.forEach(element => element.classList.toggle("btn-outline-dark"));
-            //     btnOutline.forEach(element => element.classList.toggle("btn-outline-light"));
-            // }else{
-            //     btnOutline = document.body.querySelectorAll(".btn-outline-light")
-            //     btnOutline.forEach(element => element.classList.toggle("btn-outline-dark"));
-            //     btnOutline.forEach(element => element.classList.toggle("btn-outline-light"));
-            // }
-            btnOutline.forEach(element => element.classList.toggle("btn-outline-light"));
+                btnOutline.forEach(element => element.classList.toggle("btn-outline-dark"));
+                btnOutline.forEach(element => element.classList.toggle("btn-outline-light"));
+            }else{
+                btnOutline = document.body.querySelectorAll(".btn-outline-light")
+                btnOutline.forEach(element => element.classList.toggle("btn-outline-dark"));
+                btnOutline.forEach(element => element.classList.toggle("btn-outline-light"));
+
+            }
             btnSolid.forEach(element => element.classList.toggle("btn-dark"))
 
             document.body.classList.toggle("light-theme")
             document.body.classList.toggle("dark-theme")
-            this.toggleButton.classList.toggle('btn-dark')
-            this.toggleButton.classList.toggle('btn-light')
+           
             
             this.emit("switch", this.theme);
             
